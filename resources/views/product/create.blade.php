495485f92c4a97e5a8309a393b884e7a357f4953
wap <wap>
2017-03-25 11:59:45 +0100

    bug fixes

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,102 +1,79 @@
 @extends('layouts.app')
-@section('content')
 
+@section('content')
+<section>
         <div class="container">
-        <div class="row">
+        <div class="breadcrumbs">
+                <ol class="breadcrumb">
+                  <li><a href="{{url('\login')}}">Register</a></li>
+                  <li class="active">Billing Address</li>
+                </ol>
+            </div>
+            <div class="row">
                 <div class="col-sm-8 col-sm-offset-1">
                     <div class="login-form">
                         <div class="step-one">
-                    <h2 class="heading">Add Product</h2>
-                            </div>
-                        <form  role="form" method="POST" action="{{ route('product.store') }}" enctype="multipart/form-data">
+                    <h2 class="heading">Billing Address</h2>
+                </div>
+                        <form  role="form" method="POST" action="{{ route('store_address') }}">
                         {{ csrf_field() }}
 
-                        <div class="{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="{{ $errors->has('address') ? ' has-error' : '' }}">
                            
-                                <input id="name" type="name"  name="name" value="{{ old('name') }}" placeholder = "Product Name" required autofocus>
+                                <input id="address" type="address"  name="address" value="{{ old('address') }}" placeholder = "Address" required autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('address'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('address') }}</strong>
                                     </span>
                                 @endif
                             
                         </div>
 
-                        <div class="{{ $errors->has('brand') ? ' has-error' : '' }}">
+                        <div class="{{ $errors->has('landmark') ? ' has-error' : '' }}">
                             
-                                <input id="text" type="text"  name="brand" placeholder = "Brand or manufacturer" required>
+                                <input id="text" type="text"  name="landmark" placeholder = "Opposite, Close to.." required>
 
-                                @if ($errors->has('brand'))
+                                @if ($errors->has('landmark'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('brand') }}</strong>
+                                        <strong>{{ $errors->first('landmark') }}</strong>
                                     </span>
                                 @endif
 
                         </div>
 
-
-                        <div class="{{ $errors->has('category') ? ' has-error' : '' }}">
+                        <div class="{{ $errors->has('LGA') ? ' has-error' : '' }}">
                             
-                                <select name="category">
-                                    <option disabled selected>Select Product Category</option>
-                                    <option value="Hair">Hair</option>
-                                    <option value="Face">Face</option>
-                                    <option value="Body">Body</option>
-                                    <option value="Feet">Feet</option>
-                                    <option value="Nails">Nails</option>
-                                    <option value="Perfume">Perfume</option>
+                                <select name="LGA">
+                                    <option disabled selected>Local Government Area</option>
+                                    <option value="Barkin Ladi">Barkin Ladi</option>
+                                    <option value="Bassa">Bassa</option>
+                                    <option value="Bokkos">Bokkos</option>
+                                    <option value="Jos-East">Jos-East</option>
+                                    <option value="Jos-North">Jos-North</option>
+                                    <option value="Jos-South">Jos-South</option>
+                                    <option value="Kanam">Kanam</option>
+                                    <option value="Kanke">Kanke</option>
+                                    <option value="Langtang-North">Langtang-North</option>
+                                    <option value="Langtang-South">Langtang-South</option>
+                                    <option value="Mangu">Mangu</option>
+                                    <option value="Mikang">Mikang</option>
+                                    <option value="Qua'anpan">Qua'an Pan</option>
+                                    <option value="Ryom">Ryom</option>
+                                    <option value="Shendam">Shendam</option>
+                                    <option value="Wasse">Wasse</option>
                                 </select>
-                                @if ($errors->has('category'))
+                                @if ($errors->has('landmark'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('category') }}</strong>
+                                        <strong>{{ $errors->first('landmark') }}</strong>
                                     </span>
                                 @endif
 
                         </div>
 
-                        <div style="margin-top:2%; margin-bottom:2%;"  class="{{ $errors->has('description') ? ' has-error' : '' }}">
-                            
-                                <textarea name="description" placeholder = "Describe the product"></textarea>
-                                @if ($errors->has('description'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('description') }}</strong>
-                                    </span>
-                                @endif
-
+                        <div >
+                           
                         </div>
-
-                        <div class="{{ $errors->has('price') ? ' has-error' : '' }}">
-                            
-                                <input id="price" type="price"  name="price" placeholder = "Unit Price" required>
-
-                                @if ($errors->has('price'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('price') }}</strong>
-                                    </span>
-                                @endif
-
-                        </div>
-                        <div class="{{ $errors->has('total') ? ' has-error' : '' }}">
-                            
-                                <input id="text" type="text"  name="total" placeholder = "Quantity in stock" required>
-
-                                @if ($errors->has('total'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('total') }}</strong>
-                                    </span>
-                                @endif
-
-                        </div>
-
-                       <div class="{{ $errors->has('image') ? ' has-error' : '' }}">
-                           <input type="file" name="image">
-                              @if ($errors->has('image'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('image') }}</strong>
-                                    </span>
-                                @endif
-                       </div>
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
@@ -113,5 +90,6 @@
               
             </div>
         </div>
+    </section>
 
 @endsection
