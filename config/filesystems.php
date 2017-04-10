8fae9bbc0712fb8c44ca3f7ee803d67edb0a75ed
wap <wap>
2017-04-10 11:09:37 +0100

    more debugging

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,6 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'efceefoods'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', 'C:/Users/dirnen/Downloads/efceefoods-c4b75d4f10fb.json'), // optional: /path/to/service-account.json
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'efceefoods.appspot.com'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
